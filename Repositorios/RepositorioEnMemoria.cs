be1904b17f5016f8c3853f6a5a3e1177beaab4fb
oscarmosquera91 <oscar.mosquera91@hotmail.com>
2023-10-19 10:17:14 -0500

    First commit  by git init

---
+++ Repositorios/RepositorioEnMemoria.cs
@@ -0,0 +1,23 @@
+﻿using PeliculasAPI.Entidades;
+using System.Collections.Generic;
+
+namespace PeliculasAPI.Repositorios
+{
+    public class RepositorioEnMemoria
+    {
+        private List<Genero> _generos;
+        public RepositorioEnMemoria()
+        {
+            _generos = new List<Genero>()
+            {
+                new Genero(){ Id = 1, Nombre = "Comedia" },
+                new Genero(){ Id = 2, Nombre = "Acción" }
+            };
+        }
+
+        public List<Genero> ObtenerTodosLosGeneros()
+        {
+            return _generos;
+        }
+    }
+}
